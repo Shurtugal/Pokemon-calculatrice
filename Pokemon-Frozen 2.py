9d038c2de4eae57d7fca67dd0a5aa50af38ca9f8
Shurtugal <mathias.michel99@orange.fr>
2016-01-16 23:56:50 +0100

    Starting to implement effects

--- Pokemon-Frozen 2.py
+++ Pokemon-Frozen 2.py
@@ -18,9 +18,14 @@ fighttextcount=-1
 screen=pygame.display.set_mode(size)
 pygame.display.set_caption("Pokemon Frozen")
 
-sid=109
+#pygame.mixer.music.load('Autres images\\sons/Battle theme.wav')
+#pygame.mixer.music.play(0, 0.0)
+
+wild=[5.0, 109, [0,0,0,0,0,0,1.0,1.0,1.0,1.0], 3, 110, 0, 0, 30, 30, 0, 0]
+
+sid=wild[1]
 mid=save[3]
-Wmaxpv=pokemon(sid)[3][3]*5/50+5+10
+Wmaxpv=pokemon(sid)[3][3]*wild[0]/50+wild[0]+10
 Wpv=Wmaxpv
 Smaxpv=pokemon(mid)[3][3]*save[0]/50+save[0]+10
 Spv=save[2]
@@ -36,10 +41,10 @@ while not end:
 		fighttextcount=0
 		framecount=0
 		textW1=pygame.font.SysFont('Arial', 20, False, False).render(pokemon(sid)[0], True, black)
-		textW2=pygame.font.SysFont('Arial', 20, False, False).render('n. 5', True, black)
+		textW2=pygame.font.SysFont('Arial', 20, False, False).render('n. '+str(int(wild[0])), True, black)
 		textW3=pygame.font.SysFont('Arial', 20, False, False).render('PV', True, black)
 		textS1=pygame.font.SysFont('Arial', 20, False, False).render(pokemon(mid)[0], True, black)
-		textS2=pygame.font.SysFont('Arial', 20, False, False).render('n. '+str(save[0]), True, black)
+		textS2=pygame.font.SysFont('Arial', 20, False, False).render('n. '+str(int(save[0])), True, black)
 		textS3=pygame.font.SysFont('Arial', 20, False, False).render('PV', True, black)
 		textS4=pygame.font.SysFont('Arial', 20, False, False).render(str(int(Spv))+'/'+str(int(Smaxpv)), True, black)
 		textS5=pygame.font.SysFont('Arial', 12, False, False).render('exp', True, black)
@@ -88,14 +93,17 @@ while not end:
 					fighttextcount+=1
 					framecount=0
 					alea=random.uniform(0.85,1)
-					if random.randint(1,16)==1:
-						critical=1
-					else:
-						critical=0
-					typemod=table(attaque(save[J[0]+2*J[1]+5])[0],pokemon(sid)[1])*table(attaque(save[J[0]+2*J[1]+5])[0],pokemon(sid)[2])
+					critical=0
+					typemod=1
+					if attaque(save[J[0]+J[1]*2+5])[1]>0:
+						if random.randint(1,16)==1:
+							critical=1
+						typemod=table(attaque(save[J[0]+2*J[1]+5])[0],pokemon(sid)[1])*table(attaque(save[J[0]+2*J[1]+5])[0],pokemon(sid)[2])
 					stab=1+(attaque(save[J[0]+2*J[1]+5])[0]==pokemon(mid)[1] or attaque(save[J[0]+2*J[1]+5])[0]==pokemon(mid)[2])*0.5
 					save[J[0]+2*J[1]+9]-=1
 					LA=J
+					effectdone=0
+					effect2done=0
 				else:
 					framecount=0
 					submenu=1.5
@@ -118,15 +126,15 @@ while not end:
 	if fighttextcount==3:
 		text1=pygame.font.SysFont('Arial', 35, False, False).render(pokemon(mid)[0]+' utilise '+attaque(save[J[0]+J[1]*2+5])[4]+' !', True, black)
 		if framecount in range(60,111) and attaque(save[J[0]+J[1]*2+5])[1]>0:
-			Wpv+=-((save[0]*0.4+2)*(pokemon(mid)[3][0]*save[0]/50+5)*attaque(save[J[0]+J[1]*2+5])[1]/(pokemon(sid)[3][2]*5/50+5)/50+2)*(critical+1)*typemod*stab*alea/50
+			Wpv+=-((save[0]*0.4+2)*(pokemon(mid)[3][0]*save[0]/50+5)*save[4][6]*attaque(save[J[0]+J[1]*2+5])[1]/(pokemon(sid)[3][1]*wild[0]/50+5)/wild[2][7]/50+2)*(critical+1)*typemod*stab*alea/50
 			if Wpv<=0:
 				Wpv=0
 				fighttextcount=4
 				framecount=0
-				if critical or typemod!=1:
+				if critical or typemod!=1 or attaque(save[J[0]+J[1]*2+5])[3] in [8,10,12,15,16,18,19,20,21,23,24,25,26,27,28,30,31]:
 					fighttextcount=3.1
 					framecount=0
-		if framecount==140:
+		if framecount==140-(attaque(save[J[0]+J[1]*2+5])[1]==0)*80:
 			text1=pygame.font.SysFont('Arial', 35, False, False).render('Que doit faire ', True, black)
 			text2=pygame.font.SysFont('Arial', 35, False, False).render(fighttext[2][0], True, black)
 			text3=pygame.font.SysFont('Arial', 35, False, False).render(fighttext[2][1], True, black)
@@ -134,15 +142,228 @@ while not end:
 			text5=pygame.font.SysFont('Arial', 35, False, False).render(fighttext[2][3], True, black)
 			fighttextcount=2
 			submenu=0
-			J=0,0
-			if critical or typemod!=1:
+			if critical or typemod!=1 or attaque(save[J[0]+J[1]*2+5])[3]!=0:
 				fighttextcount=3.1
 				framecount=0
+			else:
+				J=0,0
 	if fighttextcount==3.1:
+		text1b=pygame.font.SysFont('Arial', 35, False, False).render('', True, black)
 		if critical:
 			text1=pygame.font.SysFont('Arial', 35, False, False).render('Coup critique !', True, black)
 		else:
-			text1=pygame.font.SysFont('Arial', 35, False, False).render('C\'est '+typemodtext[[0,0.25,0.5,2,4].index(typemod)]+'efficace !', True, black)
+			if typemod!=1:
+				text1=pygame.font.SysFont('Arial', 35, False, False).render('C\'est '+typemodtext[[0,0.25,0.5,2,4].index(typemod)]+'efficace !', True, black)
+			else:
+				if attaque(save[J[0]+J[1]*2+5])[3]==8:
+					if save[4][6]>3.7 and not effectdone:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("L'attaque de "+pokemon(mid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus augmenter.", True, black)
+					else:
+						if save[4][6]<1 and not effectdone:
+							save[4][6]+=2*save[4][6]*save[4][6]/(4-2*save[4][6])
+							effectdone=1
+						if save[4][6]>=0.9 and not effectdone:
+							save[4][6]+=0.5
+							effectdone=1
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("L'attaque de "+pokemon(mid)[0]+" augmente.", True, black)
+				if attaque(save[J[0]+J[1]*2+5])[3]==9:
+					if wild[2][6]<0.27 and not effectdone:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("L'attaque de "+pokemon(sid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus baisser.", True, black)
+					else:
+						if wild[2][6]<=1.1 and not effectdone:
+							wild[2][6]-=2*wild[2][6]*wild[2][6]/(4+2*wild[2][6])
+							effectdone=1
+						if wild[2][6]>1 and not effectdone:
+							wild[2][6]-=0.5
+							effectdone=1
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("L'attaque de "+pokemon(sid)[0]+" baisse.", True, black)
+				if attaque(save[J[0]+J[1]*2+5])[3]==10:
+					if save[4][7]>3.7 and not effectdone:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La defense de "+pokemon(mid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus augmenter.", True, black)
+					else:
+						if save[4][7]<1 and not effectdone:
+							save[4][7]+=2*save[4][7]*save[4][7]/(4-2*save[4][7])
+							effectdone=1
+						if save[4][7]>=0.9 and not effectdone:
+							save[4][7]+=0.5
+							effectdone=1
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La defense de "+pokemon(mid)[0]+" augmente.", True, black)
+				if attaque(save[J[0]+J[1]*2+5])[3]==11:
+					if wild[2][7]<0.27 and not effectdone:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La defense de "+pokemon(sid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus baisser.", True, black)
+					else:
+						if wild[2][7]<=1 and not effectdone:
+							wild[2][7]-=2*wild[2][7]*wild[2][7]/(4+2*wild[2][7])
+							effectdone=1
+						if wild[2][7]>1 and not effectdone:
+							wild[2][7]-=0.5
+							effectdone=1
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La defense de "+pokemon(sid)[0]+" baisse.", True, black)
+				if attaque(save[J[0]+J[1]*2+5])[3]==12:
+					if save[4][8]>3.7 and not effectdone:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La vitesse de "+pokemon(mid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus augmenter.", True, black)
+					else:
+						if save[4][8]<1 and not effectdone:
+							save[4][8]+=2*save[4][8]*save[4][8]/(4-2*save[4][8])
+							effectdone=1
+						if save[4][8]>=0.9 and not effectdone:
+							save[4][8]+=0.5
+							effectdone=1
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La vitesse de "+pokemon(mid)[0]+" augmente.", True, black)
+				if attaque(save[J[0]+J[1]*2+5])[3]==13:
+					if wild[2][8]<0.27 and not effectdone:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La vitesse de "+pokemon(sid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus baisser.", True, black)
+					else:
+						if wild[2][8]<=1 and not effectdone:
+							wild[2][8]-=2*wild[2][8]*wild[2][8]/(4+2*wild[2][8])
+							effectdone=1
+						if wild[2][8]>1 and not effectdone:
+							wild[2][8]-=0.5
+							effectdone=1
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La vitesse de "+pokemon(sid)[0]+" baisse.", True, black)
+				if attaque(save[J[0]+J[1]*2+5])[3]==14:
+					if wild[2][9]<0.27 and not effectdone:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La precision de "+pokemon(sid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus baisser.", True, black)
+					else:
+						if wild[2][9]<=1 and not effectdone:
+							wild[2][9]-=2*wild[2][9]*wild[2][9]/(4+2*wild[2][9])
+							effectdone=1
+						if wild[2][9]>1 and not effectdone:
+							wild[2][9]-=0.5
+							effectdone=1
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La precision de "+pokemon(sid)[0]+" baisse.", True, black)
+				if attaque(save[J[0]+J[1]*2+5])[3]==21:
+					if save[4][6]>3.7 and framecount<40 and not effectdone:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("L'attaque de "+pokemon(mid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus augmenter.", True, black)
+					else:
+						if framecount<40:
+							if save[4][6]<1 and not effectdone:
+								save[4][6]+=2*save[4][6]*save[4][6]/(4-2*save[4][6])
+								effectdone=1
+							if save[4][6]>=0.9 and not effectdone:
+								save[4][6]+=0.5
+								effectdone=1
+							text1=pygame.font.SysFont('Arial', 35, False, False).render("L'attaque de "+pokemon(mid)[0]+" augmente.", True, black)
+					if save[4][8]>3.7 and framecount>=40 and not effect2done:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La vitesse de "+pokemon(mid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus augmenter.", True, black)
+					else:
+						if framecount>=40:
+							if save[4][8]<1 and not effect2done:
+								save[4][8]+=2*save[4][8]*save[4][8]/(4-2*save[4][8])
+								effect2done=1
+							if save[4][8]>=0.9 and not effect2done:
+								save[4][8]+=0.5
+								effect2done=1
+							text1=pygame.font.SysFont('Arial', 35, False, False).render("La vitesse de "+pokemon(mid)[0]+" augmente.", True, black)
+				if attaque(save[J[0]+J[1]*2+5])[3]==26:
+					if save[4][6]>3.7 and framecount<40 and not effectdone:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("L'attaque de "+pokemon(mid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus augmenter.", True, black)
+					else:
+						if framecount<40:
+							if save[4][6]<1 and not effectdone:
+								save[4][6]+=2*save[4][6]*save[4][6]/(4-2*save[4][6])
+								effectdone=1
+							if save[4][6]>=0.9 and not effectdone:
+								save[4][6]+=0.5
+								effectdone=1
+							text1=pygame.font.SysFont('Arial', 35, False, False).render("L'attaque de "+pokemon(mid)[0]+" augmente beaucoup.", True, black)
+					if save[4][6]>3.7 and framecount>=40 and not effect2done:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("L'attaque de "+pokemon(mid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus augmenter.", True, black)
+					else:
+						if framecount>=40:
+							if save[4][6]<1 and not effect2done:
+								save[4][6]+=2*save[4][6]*save[4][6]/(4-2*save[4][6])
+								effect2done=1
+							if save[4][6]>=0.9 and not effect2done:
+								save[4][6]+=0.5
+								effect2done=1
+							text1=pygame.font.SysFont('Arial', 35, False, False).render("L'attaque de "+pokemon(mid)[0]+" augmente beaucoup.", True, black)
+				if attaque(save[J[0]+J[1]*2+5])[3]==27:
+					if save[4][7]>3.7 and framecount<40 and not effectdone:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La defense de "+pokemon(mid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus augmenter.", True, black)
+					else:
+						if framecount<40:
+							if save[4][7]<1 and not effectdone:
+								save[4][7]+=2*save[4][7]*save[4][7]/(4-2*save[4][7])
+								effectdone=1
+							if save[4][7]>=0.9 and not effectdone:
+								save[4][7]+=0.5
+								effectdone=1
+							text1=pygame.font.SysFont('Arial', 35, False, False).render("La defense de "+pokemon(mid)[0]+" augmente beaucoup.", True, black)
+					if save[4][7]>3.7 and framecount>=40 and not effect2done:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La defense de "+pokemon(mid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus augmenter.", True, black)
+					else:
+						if framecount>=40:
+							if save[4][7]<1 and not effect2done:
+								save[4][7]+=2*save[4][7]*save[4][7]/(4-2*save[4][7])
+								effect2done=1
+							if save[4][7]>=0.9 and not effect2done:
+								save[4][7]+=0.5
+								effect2done=1
+							text1=pygame.font.SysFont('Arial', 35, False, False).render("La defense de "+pokemon(mid)[0]+" augmente beaucoup.", True, black)
+				if attaque(save[J[0]+J[1]*2+5])[3]==28:
+					if save[4][8]>3.7 and framecount<40 and not effectdone:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La vitesse de "+pokemon(mid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus augmenter.", True, black)
+					else:
+						if framecount<40:
+							if save[4][8]<1 and not effectdone:
+								save[4][8]+=2*save[4][8]*save[4][8]/(4-2*save[4][8])
+								effectdone=1
+							if save[4][8]>=0.9 and not effectdone:
+								save[4][8]+=0.5
+								effectdone=1
+							text1=pygame.font.SysFont('Arial', 35, False, False).render("La vitesse de "+pokemon(mid)[0]+" augmente beaucoup.", True, black)
+					if save[4][8]>3.7 and framecount>=40 and not effect2done:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La vitesse de "+pokemon(mid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus augmenter.", True, black)
+					else:
+						if framecount>=40:
+							if save[4][8]<1 and not effect2done:
+								save[4][8]+=2*save[4][8]*save[4][8]/(4-2*save[4][8])
+								effect2done=1
+							if save[4][8]>=0.9 and not effect2done:
+								save[4][8]+=0.5
+								effect2done=1
+							text1=pygame.font.SysFont('Arial', 35, False, False).render("La vitesse de "+pokemon(mid)[0]+" augmente beaucoup.", True, black)
+				if attaque(save[J[0]+J[1]*2+5])[3]==28:
+					if wild[2][7]<0.27 and framecount<40 and not effectdone:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La defense de "+pokemon(mid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus baisser.", True, black)
+					else:
+						if framecount<40:
+							if wild[2][7]<=1 and not effectdone:
+								wild[2][7]-=2*wild[2][7]*wild[2][7]/(4+2*wild[2][7])
+								effectdone=1
+							if wild[2][7]>1 and not effectdone:
+								wild[2][7]-=0.5
+								effectdone=1
+							text1=pygame.font.SysFont('Arial', 35, False, False).render("La defense de "+pokemon(mid)[0]+" baisse beaucoup.", True, black)
+					if wild[2][7]<0.27 and framecount>=40 and not effect2done:
+						text1=pygame.font.SysFont('Arial', 35, False, False).render("La defense de "+pokemon(mid)[0], True, black)
+						text1b=pygame.font.SysFont('Arial', 35, False, False).render("ne peut plus baisser.", True, black)
+					else:
+						if framecount>=40:
+							if wild[2][7]<=1 and not effect2done:
+								wild[2][7]-=2*wild[2][7]*wild[2][7]/(4+2*wild[2][7])
+								effect2done=1
+							if wild[2][7]>1 and not effect2done:
+								wild[2][7]-=0.5
+								effect2done=1
+							text1=pygame.font.SysFont('Arial', 35, False, False).render("La defense de "+pokemon(mid)[0]+" baisse beaucoup.", True, black)
 		if framecount>80:
 			if critical and typemod!=1:
 				fighttextcount=3.2
@@ -152,6 +373,7 @@ while not end:
 					fighttextcount=4
 				else:
 					fighttextcount=2
+					J=0,0
 	if fighttextcount==3.2:
 		text1=pygame.font.SysFont('Arial', 35, False, False).render('C\'est '+typemodtext[[0,0.25,0.5,2,4].index(typemod)]+'efficace !', True, black)
 		if framecount>80:
@@ -159,6 +381,7 @@ while not end:
 				fighttextcount=4
 			else:
 				fighttextcount=2
+				J=0,0
 	if fighttextcount==4:
 		text1=pygame.font.SysFont('Arial', 35, False, False).render(pokemon(sid)[0]+' sauvage est K.O.!', True, black)
 		if framecount>20 and pygame.key.get_pressed()[111]==1:
@@ -188,6 +411,8 @@ while not end:
 		screen.blit(textS5, [65, 72])
 	if fighttextcount in [0,1,3,3.1,3.2,4]:
 		screen.blit(text1, [140, 400])
+	if fighttextcount==3.1:
+		screen.blit(text1b, [140, 450])
 	if fighttextcount==2:
 		if submenu==0:
 			pygame.draw.rect(screen,black,[511+J[0]*171,412+J[1]*50,15,15])
@@ -211,5 +436,9 @@ while not end:
 			screen.blit(text1, [140, 400])
 	pygame.display.flip()
 	pygame.time.Clock().tick(60)
+save[4][6]=1.0
+save[4][7]=1.0
+save[4][8]=1.0
+save[4][9]=1.0
 open('save.sav','w').write(repr(save))
 sys.exit()
